69e89455078fbb9a2fb30a07008999a82dc97038
shururinguan <agestockyou@gmail.com>
2024-06-09 15:56:12 +0900

    add style

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,13 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import type { LinksFunction } from "@remix-run/node";
+import appStylesHref from "./app.css?url";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: appStylesHref },
+];
+
 export default function App() {
   return (
     <html lang="en">
